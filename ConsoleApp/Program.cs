cf40b0268bcd34b5fbd239fe1b4c3873e8619f5a
nevsjs <nevsjs@gmail.com>
2012-10-20 14:17:03 +1000

    Loaded projects

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClassLib;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1();
+            c1.Foo();
+        }
+    }
+}
